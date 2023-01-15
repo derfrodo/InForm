671afbc1924df3c5b4c1bdbdc33254b270ba376e
Stefan Pauka <paukastefan@gmail.com>
2023-01-15 12:07:36 +0100

    add test

--- packages/gen-in-form-cli/src/common/utils/resolveFileCandidates.test.ts
+++ packages/gen-in-form-cli/src/common/utils/resolveFileCandidates.test.ts
@@ -1,17 +1,18 @@
 import { getDefaultArgsSearchGeneralSettingsIntegrationTestBaseTests } from "@src/arguments/testDefaults/getDefaultArgsSearchGeneralSettingsIntegrationTestBaseTests";
 import { getGeneratorLogger } from "@src/logging/getGeneratorLogger";
+import { string } from "yargs";
 import { resolveFileCandidates } from "./resolveFileCandidates";
 
 jest.mock("@src/logging/getGeneratorLogger");
 
 describe("resolveFileCandidates module tests", () => {
-    beforeAll(() => {});
+    beforeAll(() => { });
 
     beforeEach(() => {
         jest.clearAllMocks();
     });
 
-    it("given FileSystemHelper is not mocked when called with default patterns for search integration tests when resolveFileCandidates is called then files according to existing files in testdata are resolved", async () => {
+    it("given FileSystemHelper is not mocked when called with default patterns for search integration tests when resolveFileCandidates is called then logs that files are resolved", async () => {
         const argv =
             getDefaultArgsSearchGeneralSettingsIntegrationTestBaseTests();
         const candidates = await resolveFileCandidates(
@@ -20,10 +21,49 @@ describe("resolveFileCandidates module tests", () => {
         );
         const debug = getGeneratorLogger().debug;
 
-        // Test will fail if beforeAll fails
         expect(debug).toBeCalledWith(
             `Files candidates resolved: `,
-            '{"fileCandidates":"[\\"./testdata/graphqlTests/formMappings/CreateUserMapping.ts\\",\\"./testdata/graphqlTests/formMappings/CreateUserMapping2.ts\\",\\"./testdata/graphqlTests/formMappings/FakeUserMapping.ts\\",\\"./testdata/graphqlTests/formMappings/UserMapping.ts\\",\\"./testdata/graphqlTests/formMappings/UserMapping2.ts\\",\\"./testdata/graphqlTests/generalSettings/FormGeneralSettings.ts\\",\\"./testdata/graphqlTests/graphql/generated.ts\\",\\"./testdata/graphqlTests/formMappings/CreateUserMapping2.generated.tsx\\",\\"./testdata/graphqlTests/formMappings/CreateUserMapping2Types.generated.tsx\\",\\"./testdata/graphqlTests/formMappings/CreateUserMapping2mui.generated.tsx\\",\\"./testdata/graphqlTests/formMappings/UserMapping.generated.tsx\\",\\"./testdata/graphqlTests/formMappings/UserMapping2.generated.tsx\\",\\"./testdata/graphqlTests/formMappings/UserMapping2Types.generated.tsx\\",\\"./testdata/graphqlTests/formMappings/UserMapping2mui.generated.tsx\\",\\"./testdata/graphqlTests/formMappings/UserMappingTypes.generated.tsx\\",\\"./testdata/graphqlTests/formMappings/UserMappingmui.generated.tsx\\"]"}'
+            expect.any(String)
         );
+    });
+
+    it("given FileSystemHelper is not mocked when called with default patterns for search integration tests when resolveFileCandidates is called then logged files matches expectation", async () => {
+        const argv =
+            getDefaultArgsSearchGeneralSettingsIntegrationTestBaseTests();
+        const candidates = await resolveFileCandidates(
+            argv.srcFolder,
+            argv.globalSettingsFilesPattern
+        );
+        const debug = getGeneratorLogger().debug;
+
+        expect(debug).toBeCalledWith(
+            `Files candidates resolved: `,
+            expect.any(String)
+        );
+
+        const debugMock = debug as jest.MockedFunction<typeof debug>;
+        const jsonizedData = debugMock.mock.calls[1][1];
+        const data = JSON.parse(jsonizedData);
+        const loggedcandidates = JSON.parse(data.fileCandidates);
+
+
+        expect(loggedcandidates).toEqual([
+            "./testdata/graphqlTests/formMappings/CreateUserMapping.ts",
+            "./testdata/graphqlTests/formMappings/CreateUserMapping2.ts",
+            "./testdata/graphqlTests/formMappings/FakeUserMapping.ts",
+            "./testdata/graphqlTests/formMappings/UserMapping.ts",
+            "./testdata/graphqlTests/formMappings/UserMapping2.ts",
+            "./testdata/graphqlTests/generalSettings/FormGeneralSettings.ts",
+            "./testdata/graphqlTests/graphql/generated.ts",
+            "./testdata/graphqlTests/formMappings/CreateUserMapping2.generated.tsx",
+            "./testdata/graphqlTests/formMappings/CreateUserMapping2Types.generated.tsx",
+            "./testdata/graphqlTests/formMappings/CreateUserMapping2mui.generated.tsx",
+            "./testdata/graphqlTests/formMappings/UserMapping.generated.tsx",
+            "./testdata/graphqlTests/formMappings/UserMapping2.generated.tsx",
+            "./testdata/graphqlTests/formMappings/UserMapping2Types.generated.tsx",
+            "./testdata/graphqlTests/formMappings/UserMapping2mui.generated.tsx",
+            "./testdata/graphqlTests/formMappings/UserMappingTypes.generated.tsx",
+            "./testdata/graphqlTests/formMappings/UserMappingmui.generated.tsx",
+        ]);
     });
 });
