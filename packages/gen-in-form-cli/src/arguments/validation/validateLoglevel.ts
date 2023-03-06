2d9a57b54dbbd2e2f6a2ebc47d80607c3820879e
Stefan Pauka <derfrodo0715@gmail.com>
2023-03-06 09:01:55 +0100

    use external classes (todo)

--- packages/gen-in-form-cli/src/arguments/validation/validateLoglevel.ts
+++ packages/gen-in-form-cli/src/arguments/validation/validateLoglevel.ts
@@ -1,23 +1,9 @@
 import { InFormCliArguments } from "@src/common/types/InFormCliArguments";
 import { LogLevel } from "@src/common/types/LogLevel";
 
-type KeysOfType<T, TProp> = {
-    [P in keyof T]: T[P] extends TProp ? P : never;
-}[keyof T] & string;
-
-type PropType<TObj, TProp extends keyof TObj> = TObj[TProp];
-
 
 type InFormCliArgumentsKeysOfType<T> = KeysOfType<InFormCliArguments, T>
 type InFormCliArgumentsPropertyType<TKey extends keyof InFormCliArguments> = PropType<InFormCliArguments, TKey>
-
-type ValidatePropertyFunction<T> = <
-    TKey extends keyof InFormCliArgumentsKeysOfType<T>
->(
-    obj: any,
-    propertyName: TKey
-) => obj is { [key in TKey]: InFormCliArgumentsPropertyType<TKey> };
-
 
 export function validateLoglevel<TKey extends InFormCliArgumentsKeysOfType<LogLevel>>(
     obj: any,
