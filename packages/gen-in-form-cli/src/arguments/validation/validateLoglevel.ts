27e18f1c9f72a949a4aa0a7b5881ab331451f4a7
Stefan Pauka <derfrodo0715@gmail.com>
2023-03-06 09:42:12 +0100

    validation

--- packages/gen-in-form-cli/src/arguments/validation/validateLoglevel.ts
+++ packages/gen-in-form-cli/src/arguments/validation/validateLoglevel.ts
@@ -1,9 +1,19 @@
+import { KeysOfType, PropType } from "@derfrodo/gen-in-form-runtime";
 import { InFormCliArguments } from "@src/common/types/InFormCliArguments";
 import { LogLevel } from "@src/common/types/LogLevel";
 
 
-type InFormCliArgumentsKeysOfType<T> = KeysOfType<InFormCliArguments, T>
+type InFormCliArgumentsKeysOfType<T> = KeysOfType<InFormCliArguments, T> & string
 type InFormCliArgumentsPropertyType<TKey extends keyof InFormCliArguments> = PropType<InFormCliArguments, TKey>
+
+
+
+export declare type StringKeysOfType<T extends {}, TProp, TKeyType extends string | number | symbol = string> = Required<{
+    [P in keyof T]: T[P] extends TProp ? P extends string ? P : never : never;
+}[keyof T]> & TKeyType;
+
+
+type logleveltype = StringKeysOfType<InFormCliArguments, LogLevel, number>
 
 export function validateLoglevel<TKey extends InFormCliArgumentsKeysOfType<LogLevel>>(
     obj: any,
