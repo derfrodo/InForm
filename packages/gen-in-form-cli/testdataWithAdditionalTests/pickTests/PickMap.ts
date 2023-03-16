88da313201d715540e960c7af82fa29186c390ff
Stefan Pauka <derfrodo0715@gmail.com>
2023-03-16 08:45:52 +0100

    remove wrong launch settigs

--- packages/gen-in-form-cli/testdataWithAdditionalTests/pickTests/PickMap.ts
+++ packages/gen-in-form-cli/testdataWithAdditionalTests/pickTests/PickMap.ts
@@ -3,9 +3,10 @@ import { InFormMapping } from "@derfrodo/gen-in-form-runtime";
 type SomeType = {
     prop1: string;
     prop2: string;
+    prop3: string;
 };
 
-type PickType = Pick<SomeType, "prop1">;
+type PickType = Pick<SomeType, "prop1" | "prop2">;
 
 export interface PickMap extends InFormMapping<PickType> {
     name: "PickMapped";
