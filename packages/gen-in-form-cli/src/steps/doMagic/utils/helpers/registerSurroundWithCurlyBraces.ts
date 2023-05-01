6a68bdaee17d337553dc902fead9e55d16ebaa48
Stefan Pauka <14931959+derfrodo@users.noreply.github.com>
2023-05-01 14:37:58 +0200

    change register helper

--- packages/gen-in-form-cli/src/steps/doMagic/utils/helpers/registerSurroundWithCurlyBraces.ts
+++ packages/gen-in-form-cli/src/steps/doMagic/utils/helpers/registerSurroundWithCurlyBraces.ts
@@ -1,10 +1,19 @@
-import Handlebars from "handlebars";
+import Handlebars, { HelperOptions } from "handlebars";
 
 export const registerSurroundWithCurlyBraces = (
     handlebars: typeof Handlebars
 ): void => {
-    handlebars.registerHelper("surroundWithCurlyBraces", function (text) {
-        const result = `{${text}}`;
-        return new Handlebars.SafeString(result);
-    });
+    handlebars.registerHelper(
+        "surroundWithCurlyBraces",
+        function (textOrThis: any, options: HelperOptions) {
+            if (typeof textOrThis === "string") {
+                const result = `{${textOrThis}}`;
+                return new Handlebars.SafeString(result);
+            } else {
+                const data = options.fn(textOrThis, options);
+                const result = `{${data}}`;
+                return result;
+            }
+        }
+    );
 };
